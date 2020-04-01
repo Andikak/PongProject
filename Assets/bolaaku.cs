f637ee61b8adc35fc0dc8f62885e165d862558f5
Andikak <riandputra321@gmail.com>
2020-04-01 13:58:57 +0700

    added jeda and timer

--- Assets/bolaaku.cs
+++ Assets/bolaaku.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class bolaaku : MonoBehaviour
 {
     public int speed = 30;
-
     public Rigidbody2D ball;
     // Start is called before the first frame update
     void Start()
     {
         ball.velocity = new Vector2(-1,-1) * speed;
-        // GetComponent<Rigidbody2D>().velocity = new Vector2(-1,-1) * speed;
+        
     }
 
     // Update is called once per frame
@@ -22,7 +21,13 @@ public class bolaaku : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other) {
         if(other.collider.name=="Skanan" || other.collider.name=="Skiri"){
-            GetComponent<Transform>().position = new Vector2(0,0);
+           StartCoroutine(jeda());
         }
+    }
+    IEnumerator jeda(){
+        ball.velocity = Vector2.zero;
+        ball.GetComponent<Transform>().position = Vector2.zero;
+        yield return new WaitForSeconds(1);
+        ball.velocity = new Vector2(-1,-1) * speed;
     }
 }
